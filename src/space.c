3ef6c0392fa00db620a3cb28b431491029a8b20c
Kinniken <cedric@kstudio.net>
2014-07-26 21:18:54 +0200

    Made planets with 0 presence legal (needed for native worlds)

--- src/space.c
+++ src/space.c
@@ -2169,8 +2169,8 @@ static int planet_parse( Planet *planet, const xmlNodePtr parent )
             (planet->tech==NULL), "tech" );
       /*MELEMENT( planet_hasService(planet,PLANET_SERVICE_COMMODITY) &&
             (planet->ncommodities==0),"commodity" );*/
-      MELEMENT( (flags&FLAG_FACTIONSET) && (planet->presenceAmount == 0.),
-            "presence" );
+//      MELEMENT( (flags&FLAG_FACTIONSET) && (planet->presenceAmount == 0.),
+//            "presence" );
    }
 #undef MELEMENT
 

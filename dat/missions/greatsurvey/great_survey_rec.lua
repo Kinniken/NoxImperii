f8fce0293dbb90b1b5376d94689b4b5b58bea9bd
Kinniken <cedric@kstudio.net>
2016-02-18 00:03:59 +0100

    First version of recurring GS mission

---
+++ dat/missions/greatsurvey/great_survey_rec.lua
@@ -0,0 +1,48 @@
+
+include "jumpdist.lua"
+include "numstring.lua"
+include "universe/generate_helper.lua"
+include('universe/objects/class_planets.lua')
+include "universe/live/live_universe.lua"
+include "dat/missions/greatsurvey/great_survey_utils.lua"
+
+-- Mission Details
+misn_title = "The Great Survey"
+misn_reward = "Variable, based on the interest of the planets found."
+misn_desc = "Explore planets beyond the influence of major powers."
+
+-- defines Previous Planets table
+prevPlanets = {}
+prevPlanets.__save = true
+
+
+
+title = {}  --stage titles
+text = {}   --mission text
+
+finishedtitle = "One more entry added!"
+finishedtxt = [[You follow the usual survey procedure, scanning the world from orbit and taking samples. You feed the results to the survey computer and after a few seconds its preliminary analysis appears:
+Distance from Sol: ${rewardDistance} cr
+Presence of native sapients: ${rewardNatives} cr
+Fertility for humans: ${rewardFertility} cr
+Mineral riches: ${rewardMinerals} cr
+
+Grand total: ${rewardTotal} cr
+
+You have added one more line to the great enterprise, and boosted your bank account to boot!]]
+
+function create ()
+   misn.accept()
+   landhook = hook.land ("land")
+end
+
+function land ()
+
+   local successful=handlePlanet(planet.cur(), finishedtxt)
+
+end
+
+
+function abort()
+
+end
---
+++ dat/missions/greatsurvey/great_survey_utils.lua
@@ -0,0 +1,67 @@
+
+include "jumpdist.lua"
+include "numstring.lua"
+include "universe/generate_helper.lua"
+include('universe/objects/class_planets.lua')
+include "universe/live/live_universe.lua"
+
+function computePayement(surveyedPlanet)
+
+   local reward={};
+
+   local lx,ly=surveyedPlanet.c:system():coords()
+   reward.rewardDistance=(math.floor(math.max(0,(gh.calculateDistance({x=0,y=0},{x=lx,y=ly})-600)*10)))
+
+   reward.rewardNatives=0
+
+   if (surveyedPlanet.lua.natives) then
+      reward.rewardNatives=1000
+   end
+
+   if (surveyedPlanet.lua.humanFertility>0.7) then
+      reward.rewardFertility=math.floor(surveyedPlanet.lua.humanFertility*1000)
+   else
+      reward.rewardFertility=0
+   end
+
+   if (surveyedPlanet.lua.minerals>0.5) then
+      reward.rewardMinerals=math.floor(surveyedPlanet.lua.minerals*1000)
+   else
+      reward.rewardMinerals=0
+   end
+
+   reward.rewardTotal=reward.rewardDistance+reward.rewardNatives+reward.rewardFertility+reward.rewardMinerals
+
+   return reward
+
+end
+
+function isSystemValid(system)
+   local presences=system.cur():presences()
+
+   return not presences["Empire of Terra"] and not presences["Roidhunate of Ardarshir"] and not presences["Oligarchy of Betelgeuse"]
+end
+
+function handlePlanet(surveyedPlanet,successMessage)
+
+   if isSystemValid(planet.cur():system()) then
+
+      local surveyedPlanet=planet_class.load(planet.cur())
+
+      if not surveyedPlanet:hasTag("surveyed") then
+         surveyedPlanet:addTag("surveyed")
+         surveyedPlanet:addHistory("The world was surveyed by "..player:name().." on board the "..player:ship().." as part of the Second Great Survey.")
+         generatePlanetServices(surveyedPlanet)
+         surveyedPlanet:save()
+
+         local reward=computePayement(surveyedPlanet)
+
+         tk.msg( finishedtitle, gh.format(successMessage,reward) )
+         player.pay( reward.rewardTotal )
+
+         return true
+      end
+   end
+
+   return false
+end

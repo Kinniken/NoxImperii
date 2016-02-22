1461e54b63958d215ff0c5af902fbb5af53a4aed
Kinniken <cedric@kstudio.net>
2016-02-23 00:06:23 +0100

    New attempt at fixing independent_trader spawn

--- dat/factions/spawn/independent_trader.lua
+++ dat/factions/spawn/independent_trader.lua
@@ -3,61 +3,61 @@ include("dat/factions/spawn/common.lua")
 
 -- @brief Spawns a small patrol fleet.
 function spawn_patrol ()
-    local pilots = {}
-    local r = rnd.rnd()
+  local pilots = {}
+  local r = rnd.rnd()
 
-    if r < 0.5 then
-       scom.addPilot( pilots, "Independent Trader Endeavour", 5 );
-    elseif r < 0.8 then
-       scom.addPilot( pilots, "Independent Trader Endeavour", 5 );
-       scom.addPilot( pilots, "Independent Trader Scarab", 10 );
-    else
-       scom.addPilot( pilots, "Independent Trader Endeavour", 5 );
-       scom.addPilot( pilots, "Independent Trader Scarab", 5 );
-       scom.addPilot( pilots, "Independent Trader Fish Bone", 20 );
-    end
+  if r < 0.5 then
+    scom.addPilot( pilots, "Independent Trader Endeavour", 10 );
+ elseif r < 0.8 then
+   scom.addPilot( pilots, "Independent Trader Endeavour", 10 );
+   scom.addPilot( pilots, "Independent Trader Scarab", 10 );
+ else
+   scom.addPilot( pilots, "Independent Trader Endeavour", 10 );
+   scom.addPilot( pilots, "Independent Trader Scarab", 10 );
+   scom.addPilot( pilots, "Independent Trader Fish Bone", 20 );
+ end
 
-    return pilots
+ return pilots
 end
 
 
 -- @brief Spawns a medium sized squadron.
 function spawn_squad ()
-    local pilots = {}
-    local r = rnd.rnd()
+  local pilots = {}
+  local r = rnd.rnd()
 
-    if r < 0.5 then
-       scom.addPilot( pilots, "Independent Trader Endeavour", 5 );
-       scom.addPilot( pilots, "Independent Trader Scarab", 10 );
-       scom.addPilot( pilots, "Independent Trader Scarab", 10 );
-       scom.addPilot( pilots, "Independent Trader Fish Bone", 20 );
-    elseif r < 0.8 then
-       scom.addPilot( pilots, "Independent Trader Scarab", 10 );
-       scom.addPilot( pilots, "Independent Trader Fish Bone", 20 );
-       scom.addPilot( pilots, "Independent Trader Voyager Cargo", 30 );
-       scom.addPilot( pilots, "Independent Trader Hauler", 40 );
-       scom.addPilot( pilots, "Independent Trader Scarab", 10 );
-    else
-       scom.addPilot( pilots, "Independent Trader Fish Bone", 20 );
-       scom.addPilot( pilots, "Independent Trader Voyager Cargo", 30 );
-       scom.addPilot( pilots, "Independent Trader Hauler", 50 );
-       scom.addPilot( pilots, "Independent Trader Whale", 80 );
-       scom.addPilot( pilots, "Independent Trader Rhino", 120 );
-       
-    end
+  if r < 0.5 then
+   scom.addPilot( pilots, "Independent Trader Endeavour", 10 );
+   scom.addPilot( pilots, "Independent Trader Scarab", 10 );
+   scom.addPilot( pilots, "Independent Trader Scarab", 10 );
+   scom.addPilot( pilots, "Independent Trader Fish Bone", 20 );
+ elseif r < 0.8 then
+   scom.addPilot( pilots, "Independent Trader Scarab", 10 );
+   scom.addPilot( pilots, "Independent Trader Fish Bone", 20 );
+   scom.addPilot( pilots, "Independent Trader Voyager Cargo", 30 );
+   scom.addPilot( pilots, "Independent Trader Hauler", 40 );
+   scom.addPilot( pilots, "Independent Trader Scarab", 10 );
+ else
+   scom.addPilot( pilots, "Independent Trader Fish Bone", 20 );
+   scom.addPilot( pilots, "Independent Trader Voyager Cargo", 30 );
+   scom.addPilot( pilots, "Independent Trader Hauler", 50 );
+   scom.addPilot( pilots, "Independent Trader Whale", 80 );
+   scom.addPilot( pilots, "Independent Trader Rhino", 120 );
 
-    return pilots
+ end
+
+ return pilots
 end
 
 
 -- @brief Creation hook.
 function create ( max )
-    local weights = {}
+  local weights = {}
 
     -- Create weights for spawn table
     weights[ spawn_patrol  ] = 100
     weights[ spawn_squad   ] = math.max(1, -100 + 1.00 * max)
-   
+
     -- Create spawn table base on weights
     spawn_table = scom.createSpawnTable( weights )
 
@@ -65,18 +65,18 @@ function create ( max )
     spawn_data = scom.choose( spawn_table )
 
     return scom.calcNextSpawn( 0, scom.presence(spawn_data), max )
-end
+  end
 
 
 -- @brief Spawning hook
 function spawn ( presence, max )
-    local pilots
+  local pilots
 
     -- Over limit
     if presence > max then
-       return 5
-    end
-  
+     return 5
+   end
+
     -- Actually spawn the pilots
     pilots = scom.spawn( spawn_data )
 
@@ -84,4 +84,4 @@ function spawn ( presence, max )
     spawn_data = scom.choose( spawn_table )
 
     return scom.calcNextSpawn( presence, scom.presence(spawn_data), max ), pilots
-end
+  end

835e0d97c5991619e3fc45ae0b3152e7fbcbb8c6
Kinniken <cedric@kstudio.net>
2016-10-08 18:25:04 +0200

    Random names for more fleets

--- dat/fleet_names.lua
+++ dat/fleet_names.lua
@@ -1,4 +1,4 @@
-local human_generic={"Fair Ireland","Belle France","Roach","City Of Light","Nightwish","Free Trade","Headwinds","Gravity Well","Endless Sky","Escape Velocity","Nova","Galactic Plane","Orion","Nebula","Great Beyond","Long Night","Civilisation","Alpha Centauri","Light Wave","Total Eclipse","Blue Ribbon","Manifest Destiny","Black Hole","Rosetta","Philae","Pharaoh","Space Czar","Space Camel","Dune","Second Foundation","2001","Space Odyssey","Ulysses","Iliad","Three Musketeers","Montecristo","Setting Sun","Hard Radiation","Stormy Night","Uplift","Three Monkeys","Shockwave","Golden Harp","Naev","Hard Times","Citizen Kane","Blue Mars","Totoro","Dire Straits","Arctic Monkeys","Caravane Passe","Big Apple","Big Mango","New Horizon","Sweet Dreams","Master Of Orion","Jon Snow","Khaleesi","Atlantis","Laughing Gull","Blue Moon","Pink Moon","Isis","Rha","Thet","Orus","Zeus","Poseidon","Hermes","Aphrodite","Jupiter","Mars","Lakshmi","Vishnu","Ganesh","St George","Gavroche","Hard Times","Two Cities","Nougaro","Hamlet","King Lear","Metamorphosis","Gilgamesh","Second Chance","Strange Tides","Starlight","Drama Queen","Anubis","Baal","Horus","Ishtar","Fenghuang","Tianlong","Jiuwei Hu","Amaterasu","Agni","Vishnu","Parvati","Saraswati","Cernunnos","Belenos","Vulcan","Hercules","Freyr","Odin","Xolotl","Quetzalcoatl","Itzamna"}
+local human_generic={"Fair Ireland","Belle France","Roach","City of Light","Nightwish","Free Trade","Headwinds","Gravity Well","Endless Sky","Escape Velocity","Nova","Galactic Plane","Orion","Nebula","Great Beyond","Long Night","Civilisation","Alpha Centauri","Light Wave","Total Eclipse","Blue Ribbon","Manifest Destiny","Black Hole","Rosetta","Philae","Pharaoh","Space Czar","Space Camel","Dune","Second Foundation","2001","Space Odyssey","Ulysses","Iliad","Three Musketeers","Montecristo","Setting Sun","Hard Radiation","Stormy Night","Uplift","Three Monkeys","Shockwave","Golden Harp","Naev","Hard Times","Citizen Kane","Blue Mars","Totoro","Dire Straits","Arctic Monkeys","Caravane Passe","Big Apple","Big Mango","New Horizon","Sweet Dreams","Master of Orion","Jon Snow","Khaleesi","Atlantis","Laughing Gull","Blue Moon","Pink Moon","Isis","Rha","Thet","Orus","Zeus","Poseidon","Hermes","Aphrodite","Jupiter","Mars","Lakshmi","Vishnu","Ganesh","St George","Gavroche","Hard Times","Two Cities","Nougaro","Hamlet","King Lear","Metamorphosis","Gilgamesh","Second Chance","Strange Tides","Starlight","Drama Queen","Anubis","Baal","Horus","Ishtar","Fenghuang","Tianlong","Jiuwei Hu","Amaterasu","Agni","Vishnu","Parvati","Saraswati","Cernunnos","Belenos","Vulcan","Hercules","Freyr","Odin","Xolotl","Quetzalcoatl","Itzamna"}
 
 local authors = {"Agatha Christie","Alain-Fournier","Albert Camus","Albert Cohen","Alberto Moravia","Aldous Huxley","Aleksandr Solzhenitsyn","Geoffrey Chaucer","Andre Breton","Andre Franquin","Andre Gide","Andre Malraux","Anne Frank","Paul Celan","Antonin Artaud","Arthur Conan Doyle","Johann Wolfgang von Goethe","Bertolt Brecht","Blaise Cendrars","Boris Vian","Louis-Ferdinand Celine","Chinua Achebe","Claude Levi-Strauss","Colette","D. H. Lawrence","Dante Alighieri","Miguel de Cervantes","Dino Buzzati","Gabriel Garcia Marquez","Franz Kafka","Edgar P. Jacobs","Ernest Hemingway","Giovanni Boccaccio","Ralph Ellison","Eugene Ionesco","Denis Diderot","F. Scott Fitzgerald","Federico Garcia Lorca","James Joyce","Francois Mauriac","Francoise Sagan","Anton Chekhov","Joseph Conrad","George Orwell","Georges Bernanos","Georges Perec","Georges Simenon","Samuel Beckett","Goscinny","Guillaume Apollinaire","Fyodor Dostoevsky","Alfred Doblin","H. G. Wells","Hannah Arendt","Hans Christian Andersen","Heinrich Boll","Hemingway","William Faulkner","Henry Miller","Herge","Joao Guimaraes Rosa","Euripides","Honore de Balzac","Hugo Pratt","Ismail Kadare","Lu Xun","J. D. Salinger","J. M. G. Le Clezio","J. R. R. Tolkien","Jack Kerouac","Jack London","Jacques Lacan","Jacques Prevert","James Hadley Chase","Jane Austen","Jean Genet","Jean Giono","Jean-Paul Sartre","John Dos Passos","John Steinbeck","Naguib Mahfouz","Jorge Luis Borges","Jules Renard","Julien Gracq","George Eliot","Astrid Lindgren","Thomas Mann","Louis Aragon","Nikolai Gogol","Luigi Pirandello","Malcolm Lowry","Marcel Proust","Margaret Mitchell","Marguerite Duras","Marguerite Yourcenar","Michel Butor","Knut Hamsun","Michel Foucault","Michel Tournier","Emily Bronte","Mikhail Bulgakov","Milan Kundera","Homer","Gunter Grass","Nathalie Sarraute","Gustave Flaubert","Henrik Ibsen","Paul Claudel","Paul Eluard","Primo Levi","Rainer Maria Rilke","Charles Dickens","Ray Bradbury","Raymond Chandler","Raymond Queneau","Rene Char","Robert Musil","Roland Barthes","Kalidasa","Yasunari Kawabata","Saint-Exupery","Saint-John Perse","Salman Rushdie","Selma Lagerlof","Sigmund Freud","Simone de Beauvoir","Giacomo Leopardi","Stefan Zweig","Doris Lessing","Nikos Kazantzakis","Uderzo","Umberto Eco","Herman Melville","Vercors","Michel de Montaigne","Virginia Woolf","Vladimir Nabokov","Elsa Morante","Toni Morrison","Halldor Laxness","William Styron","Murasaki Shikibu","Ovid","Fernando Pessoa","Edgar Allan Poe","Francois Rabelais","Juan Rulfo","Rumi","Saadi","Tayeb Salih","Jose Saramago","William Shakespeare","Sophocles","Stendhal","Laurence Sterne","Italo Svevo","Jonathan Swift","Leo Tolstoy","Mark Twain","Valmiki","Virgil","Vyasa","Walt Whitman","Jules Verne", "Arthur Rimbaud","Brian Aldiss","Poul Anderson","Isaac Asimov","J. G. Ballard","Rene Barjavel","Alfred Bester","James Blish","Pierre Boulle","Ben Bova","Ray Bradbury","Fredric Brown","John Brunner","Arthur C. Clarke","Philip K. Dick","Philip Jose Farmer","Edmond Hamilton","Robert A. Heinlein","Frank Herbert","Aldous Huxley","Ursula Le Guin","H. P. Lovecraft","Anne McCaffrey","George Orwell","Robert Silverberg","Clifford D. Simak","Norman Spinrad","Theodore Sturgeon","A. E. van Vogt","Kurt Vonnegut","John Wyndham"}
 
@@ -14,11 +14,35 @@ local states = {"United States","European Union","China","Japan","Germany","Unit
 
 local imperial_nova = {"Victory","Astute","Ambush","Artful","Trenchant","Talent","Triumph","Vanguard","Victorious","Vigilant","Vengeance","Bulwark","Daring","Dauntless","Dragon","Defender","Archer","Biter","Smiter","Pursuer","Blazer","Dasher","Charger","Scimitar","Sabre","Enterprise","Protector","Acute","Adamant","Admirable","Adversary","Aggressor","Allegiance","Arbiter","Argonaut","Arrogant","Audacious","Austere","Bellerophon","Belzebub","Bruiser","Brave","Centaur","Cerberus","Challenger","Charger","Colossus","Conflict","Conqueror","Courageous","Crown","Daring","Dauntless","Defiance","Destruction","Devastation","Diligent","Discovery","Dreadnought","Endeavour","Endurance","Fearless","Formidable","Fortitude","Furious","Gladiator","Guardian","Havock","Hazard","Herald","Hostile","Hunter","Illustrious","Implacable","Impregnable","Invincible","Onslaught","Paladin","Patrician","Resolute","Scourge","Sovereign","Splendid","Superb","Temeraire","Templar","Tormentor","Triumph","Valorous"}
 
+local barbarians_ships = {"Mighty Conan","Great Warrior","Loot Bringer","Red Dragon","Captive Taker","Might Makes Right","Chain Bearer","Axe of #","Sword of #","Clan #","Chief #","Axe of #","Sword of #","Clan #","Chief #","Axe of #","Sword of #","Clan #","Chief #"}
+
+local barbarians_random = {"Kudaan","Gamcha","Kako","Etharic","Atila","Cuixcuati","Jegutu","Khatan","Natantai","Acauhli","Jemei","Quettetli","Taicharjin","Yurkhu","Mizal","Adasreiks","Ehvana","Jatan","Ameni","Antigis","Iltlalna","Adanareiks","Bulei","Natzimoc","Chixiqui","Ambekha","Atlnexyotl","Bargugei","Dagvinta","Taichardai","Alager","Khuki","Erritu","Zonoch","Barsu","Bekkhutai","Icpallini","Fridalric","Boreri","Ermamudric","Jurba","Nacaeyotl","Bekgukha","Edalcer","Icxiton","Notlahuanal","Mapaltia","Iccatl","Chillani","Tletliten","Mungchu","Autharic","Teke","Audvakrri","Adasgern","Erdonaric","Kuratai","Ferthafuns","Chaguchu","Eidvakr","Netlaca","Alinca","Ictli","Yaotlhili","Mungchar","Totlatlatli","Eudevinth","Neitajin","Xiuhnomoc","Yalocle","Tlazal","Motlilinal","Errewin","Dagthi","Tokhan","Atharicreiks","Amemuth","Jelcha","Ixtlitli","Taimakha","Oridai","Taitan","Emiric","Atlchipil","Khugur","Xixictli","Sukela","Neimuto","Tekiurtai","Adiulfric","Iuitlhuihua","Athalaric","Eirenata","Macatl","Nochtli","Ethagild","Ehreiksla","Huinexliz","Kuyil","Berala"}
+
 local imperial_fighter_squadrons = {"Shark","Orca","Barracuda","Piranha","Sturgeon","Eel","Swordfish","Stingray"}
 
 local roman_numbers = {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV"}
 
-local pirate_names={"Blackbeard","Raptor","Dirty Harry","Scimitar","Jolly Roger","Black Pearl","Hard Radiation","Raging Sun","Crimson Tide","Screeching Harpy","Space Hawk","Burning Nova","Black Hole","Jack's Vengeance","Heavy Metal","Pirate Lord","Rapid Fire","Present Danger","Angry Bee","Kraken","Flying Dutchman","Use Of Weapons"}
+local pirate_names={"Blackbeard","Raptor","Dirty Harry","Scimitar","Jolly Roger","Black Pearl","Hard Radiation","Raging Sun","Crimson Tide","Screeching Harpy","Space Hawk","Burning Nova","Black Hole","Jack's Vengeance","Heavy Metal","Pirate Lord","Rapid Fire","Present Danger","Angry Bee","Kraken","Flying Dutchman","Use of Weapons"}
+
+local ardar_ships={"Lord #","Prince #","Admiral #","Roidhun #","Arms of #","# the Strong","# the Wise","# the Mighty","Shield of #","Sword of #"}
+
+local ardar_random={"Merrioloch","Mordan","Gelgelch","Teltioch","Urruloch","Urrioch","Brydwyr","Datolchan","Morloch","Teldwan","Urwyr","Qanriowyr","Qandan","Kraiwyr","Daryf","Gelte","Bryrioloch","Dagelch","Urdudiolch","Dakraich","Gelryf","Bryrudan","Brechrutioch","Roitioch","Mowyr","Tachtioch","Ydwan","Merte","Krairydiolch","Fotiodwan","Brechwyr","Roidwyrioch","Gelloch","Mornaryf","Darudaich","Mertiotolch","Merwyr","Kraitiodaich","Kraitoldan","Qandudwyr","Urdudiolch","Roidaich","Kraidiolch","Teldwyrioch","Chydloch","Chydrukraich","Dadwykraich","Danadwan","Urdiowyr","Merryf","Chydriodwan","Brechtioch","Fotiochan","Ywyr","Kraigelch","Urloch","Telrute","Urtoldwyr","Chydchan","Morryf","Roitoldwan","Roitioryf","Datolch","Roigelch","Urdwytolch","Roirute","Modan","Fonachan","Chydtolch","Brechgelch","Geldudwan","Fogelch","Yrywyr","Forydwyr","Qanrioch","Kraitioch","Dadwyr","Morte","Chyddwan","Darioch","Krairyloch","Roinatioch","Yriotioch","Morte","Kraigelch","Morioch","Telrugelch","Telwyr","Gelriodan","Krairutolch","Geldugelch","Gelrytolch","Chydruloch","Chydriodaich","Merwyr","Fodaich","Roichan","Chydkraich","Fodutioch","Roinadwyr"}
+
+local ardar_nova_names={"Ardar Pride","Ardar Might","Ardar Strength","Ardar Destiny","Ardar Purpose","Ardar Unity","Ardar Prerogative","Ardar Virtue","Ardar Fortitude","Ardar Power","Ardar Resilience","Ardar Discipline"}
+
+local ardar_civilian_ships={"Pride of #","Honour of #","Loyalty of #","Fealty to #","County of #","Star of #","# Mountain","Lake #", "Peak #","River #","City of #","Loyal #"}
+
+local independent_militia_ships={"Mailed Fist","Hard Nail","Tough Gun","Iron Shield","People’ Protector","People’ Shield","Town Sheriff","Rigid Views","Space Patrol","Guardian Angel","Brutal","Honest","Mighty Fist","Champion","Spear","Crossbow","Howitzer","Defender","Scimitar","Longsword","Armour","Poleaxe","Longbow"}
+
+local betelgeuse_random={"Dovalmia","Pravalzia","Connipia","Muavalzia","Venina","Fosvarzi","Fosvalpia","Saltelzi","Convalgiore","Porvalzia","Camvalsta","Conpagiore","Fosvarzia","Copazoggia","Camvargiore","Portelsta","Muapana","Chionimia","Prarazia","Camnimia","Dovalzia","Vevarmia","Prarazia","Mivarmia","Muavarza","Camvalgiore","Porvarpia","Mipana","Portelzia","Corana","Dovalzi","Mivarpia","Minizi","Dorazi","Vepogiore","Porpazia","Vepogiore","Salpasta","Muanizi","Venina","Contelna","Fospamia","Muarana","Muanimia","Camvalmia","Salnimia","Dotelzi","Salpomia","Corazi","Pratelza","Convarpia","Veniria","Salnipia","Dovalzi","Praraza","Salpazi","Chiovarzi","Fosnipia","Conpozia","Chioniza","Mipomia","Muatelsta","Porvarzi","Mipasta","Mitelzi","Mipasta","Porvalna","Dovarsta","Praporia","Coniria","Conpomia","Salpoza","Prarana","Vevalpia","Campozi","Veporia","Porvarzia","Porvarza","Donigiore","Camnigiore","Covalsta","Mivalzia","Muavarmia","Chiopozi","Campopia","Venizia","Vevarria","Cotelria","Salnipia","Salpazia","Salvarmia","Minizia","Dopamia","Convalzia","Corazia","Convalna","Fospasta","Muatelpia","Porvarzia","Salpamia"}
+
+local betelgeuse_ships={"Lady #","Fair #","Lovely #","Lucky #","Damsel #","Sister #","Dream of #","Beauty of #","Thoughts of #","Memories of #","Fortunate #","Dreaming #","Thoughtful #","Faithfull #"}
+
+local ixum_random = {"Minchnirla","More","Medis","Negera","Baxumso","Hoga","Arya","Dabata","Berrar","Konqos","Chegera","Yedar","Tulbisa","Ame","Hayqmera","Balen","Milba","Wobatsa","Gorarko","Turlem","Ginet","Walbri","Baba","Duhit","Ararla","Shasbor","Tiranma","Deriam","Bonko","Datemlo","Debi","Bako","Wabo","Walsona","Tenshof","Bilemcha","Yewash","Jinki","Limmeso","Ambara","Ajenta","Bongame","Maya","Zelen","Zeranvo","Kadarma","Delde","Awa","Wuxum","Biba","Kozan","Demlulo","Mebri","Chenriam","Wacbo","Zikem","Kobona","Chengash","Gamtu","Gowa","Hayqmi","Tulwa","Menmu","Maywash","Webol","Nedar","Wolder","Dagash","Bota","Made","Millen","Bemi","Wolwaru","Dare","Ghimla","Chenbe","Nelaile","Chenqos","Chebe","Wumen","Badsae","Bosona","Limbarqle","Chenkolelo","Ako","Mepi","Yele","Gondet","Tiple","Fimi","Filem","Dargest","Gogi","Deya","Dami","Minchlam","Checara","Badhir","Zemido","Medisle"}
+
+local royal_ixum_military = {"Paladin #", "Crown of #", "Fort #", "Castle #", "Sir #", "Duke #", "His Majesty #", "Lord #", "Arms of #", "Knight #"}
+
+local holy_flame_military = {"Holy Flame","Thundering Virtue","Eternal Gaze","Holy Writ","Steady Virtue","Sacred Writ","Holy Verse","Eternal Flame","Virtuous Guardian","Flaming Warrior","Holy Sacrifice","Noble Cause","Shining Light","Eternal Light","Holy Song","Divine Power","Bane of Heretics","Faith Power","Thundering Verse","Divine Prophecy","Flaming Sword","Holy Guardian","Divine Anger","Burning Flame","Holy War","Blessed Warrior","Wrath of The Flame"}
 
 function human_civilian()
 	local list
@@ -63,6 +87,10 @@ function imperial_navy_nova()
 	return 'ITN '..imperial_nova[math.random(#imperial_nova)]
 end
 
+function independent_militia()
+	return independent_militia_ships[math.random(#independent_militia_ships)]
+end
+
 function pirate()
 	local r=math.random()
 
@@ -97,4 +125,66 @@ function naev_pirate_name()
          return descriptor .. " " .. colour .. " " .. actor
       end
    end
+end
+
+function barbarians()
+	local name=barbarians_ships[math.random(#barbarians_ships)]
+  
+  if name:find("#") then
+    name=name:gsub("#",barbarians_random[math.random(#barbarians_random)])
+  end
+  
+  return name
+end
+
+function ardar_military()
+	local name=ardar_ships[math.random(#ardar_ships)]
+  
+  if name:find("#") then
+    name=name:gsub("#",ardar_random[math.random(#ardar_random)])
+  end
+  
+  return 'ARN '..name
+end
+
+function ardar_nova()
+  return 'ARN '..ardar_nova_names[math.random(#ardar_nova_names)]
+end
+
+function ardar_civilian()
+	local name=ardar_civilian_ships[math.random(#ardar_civilian_ships)]
+  
+  if name:find("#") then
+    name=name:gsub("#",ardar_random[math.random(#ardar_random)])
+  end
+  
+  return 'ACN '..name
+end
+
+function betelgeuse_civilian()
+	local name=betelgeuse_ships[math.random(#betelgeuse_ships)]
+  
+  if name:find("#") then
+    name=name:gsub("#",betelgeuse_random[math.random(#betelgeuse_random)])
+  end
+  
+  return name
+end
+
+function betelgeuse_military()
+  return 'BN '..betelgeuse_civilian()
+end
+
+function royal_ixum()
+	local name=royal_ixum_military[math.random(#royal_ixum_military)]
+  
+  if name:find("#") then
+    name=name:gsub("#",ixum_random[math.random(#ixum_random)])
+  end
+  
+  return 'RIN '..name
+end
+
+function holy_flame()
+  return 'HFN '..holy_flame_military[math.random(#holy_flame_military)]
 end
